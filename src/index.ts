8a1510ca6072313d41754a28dadd9efc6f63dcf7
James Towles <james.towles@gmail.com>
2016-11-29 22:44:11 -0500

    Testing different export ideas3

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,21 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
-
-import { ODataService } from './odata.service';
 import { ODataConfigService, ODataConfigServiceConfig } from './odata-config.service';
-import { ODataQuery, PagedResult } from './odata-query';
-import { ODataServiceFactory } from './odata.service.factory';
-import { ODataGetOperation } from './odata-operation';
+
+
+
 
 export { ODataService } from './odata.service';
 export { ODataConfigService, ODataConfigServiceConfig } from './odata-config.service';
-export { ODataQuery, PagedResult } from './odata-query';
 export { ODataServiceFactory } from './odata.service.factory';
+export { ODataQuery, PagedResult } from './odata-query';
 export { ODataGetOperation } from './odata-operation';
 
 
 @NgModule({
     imports: [],
-    declarations: [
-        ODataService,
-        ODataConfigService,
-        ODataQuery,
-        PagedResult,
-        ODataServiceFactory,
-        ODataGetOperation],
-    exports: [
-        ODataService,
-        ODataConfigService,
-        ODataQuery,
-        PagedResult,
-        ODataServiceFactory,
-        ODataGetOperation
-    ],
-    providers: [ODataService]
+    declarations: [],
+    exports: [],
+    providers: [ ODataConfigService ]
 })
 export class ODataModule {
     constructor(@Optional() @SkipSelf() parentModule: ODataModule) {
